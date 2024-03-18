7ba09d950630926e1cfa438d82a84cfb65d2a22f
Vitor Hirt <zorinos.masterzorin@outlook.com>
2024-03-18 16:58:09 -0300

    Update: 18/03/2024

--- Frontend/Ismartic_Frontend/src/Components/Header/header.jsx
+++ Frontend/Ismartic_Frontend/src/Components/Header/header.jsx
@@ -1,7 +1,44 @@
 function Header(){
   return(
     <>
-      
+     <div>
+        <div>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </div>
+        <div>
+          <h1>Ismartic <br/>Tecnology</h1>
+        </div>
+        <div>
+          <span>.</span>
+          <span>.</span>
+        </div>
+     </div>
+     <div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Page Blog</li>
+            <li>Search</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+     </div>        
+     <div>
+        <a href="#">
+          Sing Up
+        </a>
+        <a href="#">
+          Add Listing
+        </a>
+     </div>
+     <div>
+        <button>
+          <i class="fas fa-shopping-cart"></i>
+        </button>
+     </div>  
     </>
   )
 };
