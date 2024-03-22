2b887fbcdf61e37df01eccc085abcbe8e9d24130
Vitor Hirt <zorinos.masterzorin@outlook.com>
2024-03-22 15:51:14 -0300

    Update: 22/03/2024

--- Frontend/Ismartic_Frontend/src/App.jsx
+++ Frontend/Ismartic_Frontend/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './Normalize/Normalize.css'
 import Header from './Components/Header/header'
+import Banner from './Components/Banner/banner'
 
 function App() {
   return (
     <>
       <Header/>
+      <Banner/>
     </>
   )
 }
---
+++ Frontend/Ismartic_Frontend/src/Components/Banner/banner.jsx
@@ -0,0 +1,38 @@
+const Banner = () => {
+  return (
+
+    <>
+      <div class="banner-box">
+        <div>
+          <div class="block-box-left">
+            <div class="banner-left-box">
+              <h1>Your Trust is Our<br/> 
+              Greatest Incentive  
+              </h1>
+              <h4>There are many variations There are many variations There are many variations
+              There are many variations There are many variations.
+              </h4>
+            </div>
+            <div class="banner-right-box">
+              <button>Make Appoinment</button>
+              <h4>Support Contact</h4>
+              <h4><i class="fas fa-phone-square-alt"></i><span>083 2354 1454</span></h4>
+            </div>
+          </div>
+        </div>
+        <div class="medicArea">
+          <img src="../../../public/medic.png" alt="" />
+        </div>
+      </div>
+      <div class="banner-bottom-box">
+        <h1>Why You Should Trust Us?<br/>
+        Get Know About Us  
+        </h1>
+      </div>
+    </>
+
+  )
+}
+
+
+export default Banner;
